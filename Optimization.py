e0a0b065e72faeec9e59917414379af6791a43d3
Almir Menezes da Cunha Júnior <almir.cunha@incognia.com>
2023-08-15 22:57:01 -0300

    Add a mutation step for each gene

--- Optimization.py
+++ Optimization.py
@@ -8,7 +8,8 @@ class Optimization:
     def __init__(self, population_size: int, dimension: int, c: float):
         self.population_size = population_size
         self.dimension = dimension
-        self.learning_rate = 1/math.sqrt(self.dimension)
+        self.global_learning_rate = 1/math.sqrt(2*self.dimension)
+        self.fine_tuning_learning_rate = 1/math.sqrt(2*math.sqrt(self.dimension))
         self.min_mutation_step = 1
         self.c = c
         self.successful_mutations = 0
@@ -21,8 +22,8 @@ class Optimization:
             for _ in range(self.dimension):
                 gene = random.uniform(min_gene, max_gene)
                 chromosome.append(gene)
-            mutation_step = self.min_mutation_step
-            chromosome.append(mutation_step)
+            for _ in range(self.dimension):
+                chromosome.append(self.min_mutation_step)
             self.population.append(chromosome)
 
     def fitness(self):
@@ -37,55 +38,47 @@ class Optimization:
         return parents
     
     def crossover(self, parents: tuple[list[float], list[float]]):
-        child = []
-        for i in range(self.dimension+1):
+        offspring = []
+        for i in range(2*self.dimension):
             gene = random.choice([parents[0][i], parents[1][i]])
-            child.append(gene)
+            offspring.append(gene)
         
-        return child
+        return offspring
     
     def mutate(self, chromosome: list[float]):
-        mutation_step = chromosome[-1]
+        mutation_steps = chromosome[self.dimension:]
+        mutated_mutation_steps = []
+        for mutation_step in mutation_steps:
+            mutated_mutation_step = mutation_step * math.exp(self.global_learning_rate*random.gauss(0, 1) + self.fine_tuning_learning_rate*random.gauss(0, 1))
+            
+            if mutated_mutation_step < self.min_mutation_step:
+                mutated_mutation_step = self.min_mutation_step
+        
+            if self.get_mutation_success_rate() > 1/5:
+                mutated_mutation_step = mutated_mutation_step / self.c
+            if self.get_mutation_success_rate() < 1/5:
+                mutated_mutation_step = mutated_mutation_step * self.c
+
+            mutated_mutation_steps.append(mutated_mutation_step)
+        
         params = chromosome[:self.dimension]
-
-        mutated_mutation_step = mutation_step * math.exp(random.gauss(0, self.learning_rate))
-
-        if mutated_mutation_step < self.min_mutation_step:
-            mutated_mutation_step = self.min_mutation_step
+        mutated_params = [
+            min(max(gene+random.gauss(0, mutation_step), self.gene_lower_bound), self.gene_upper_bound) 
+            for gene, mutation_step in zip(params, mutated_mutation_steps)
+        ]
         
-        if self.get_mutation_success_rate() > 1/5:
-            mutated_mutation_step = mutated_mutation_step / self.c
-        if self.get_mutation_success_rate() < 1/5:
-            mutated_mutation_step = mutated_mutation_step * self.c
-        
-        bounded_mutation = lambda x: min(max(x, self.gene_lower_bound), self.gene_upper_bound)
-        mutated_params = [bounded_mutation(x+random.gauss(0, mutated_mutation_step)) for x in params]
-        mutated_chromosome = mutated_params+[mutated_mutation_step]
+        return mutated_params+mutated_mutation_steps
 
-        return mutated_chromosome
-
-    def survival_selection(self, children: 'list[list[float]]', μ: int) -> None:
-        children.sort(key=self.fitness)
+    def survival_selection(self, offspring: 'list[list[float]]', μ: int) -> None:
+        offspring.sort(key=self.fitness)
         self.population.sort(key=self.fitness)
-        self.population[-μ:] = children[:μ]
+        self.population[-μ:] = offspring[:μ]
 
-    def survival_selection_elitist(self, children: 'list[list[float]]', μ: int) -> None:
-        children.sort(key=self.fitness)
-        self.population.extend(children[:μ])
+    def survival_selection_elitist(self, offspring: 'list[list[float]]', μ: int) -> None:
+        offspring.sort(key=self.fitness)
+        self.population.extend(offspring[:μ])
         self.population.sort(key=self.fitness)
         self.population = self.population[:len(self.population)-μ]
-
-    def best_solution(self):
-        self.population.sort(key=self.fitness)
-        best_solution = self.fitness(self.population[0])
-
-        return self.population[0], best_solution
-    
-    def fitness_statistics(self):
-        fitness_population = []
-        for individuo in self.population:
-            fitness_population.append(self.fitness(individuo))
-        return np.mean(fitness_population), np.std(fitness_population)
     
     def update_success_rate(self, parents: list[list[float]], offspring: list[float]):
         offspring_fitness = self.fitness(offspring)
@@ -101,3 +94,15 @@ class Optimization:
         if self.total_mutations == 0:
             return 0
         return self.successful_mutations / self.total_mutations
+    
+    def best_solution(self):
+        self.population.sort(key=self.fitness)
+        best_solution = self.fitness(self.population[0])
+
+        return self.population[0], best_solution
+    
+    def fitness_statistics(self):
+        fitness_population = []
+        for individuo in self.population:
+            fitness_population.append(self.fitness(individuo))
+        return np.mean(fitness_population), np.std(fitness_population)
